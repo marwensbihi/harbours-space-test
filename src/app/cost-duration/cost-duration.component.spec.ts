ba035eac62033ab7124625f311956da88a7b58fa
marwensbihi <marwensbihi@gmail.com>
2024-05-14 10:43:28 +0100

    retrive data with http client and bind all the information in the html page. adding unit test for testing the values

--- src/app/cost-duration/cost-duration.component.spec.ts
+++ src/app/cost-duration/cost-duration.component.spec.ts
@@ -1,28 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { CostDurationComponent } from './cost-duration.component';
 import { ApiCallService } from '../shared/api-call.service';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 describe('CostDurationComponent', () => {
   let component: CostDurationComponent;
   let fixture: ComponentFixture<CostDurationComponent>;
+  let apiCallServiceSpy: jasmine.SpyObj<ApiCallService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('ApiCallService', ['getData']);
+
     await TestBed.configureTestingModule({
-      declarations: [ CostDurationComponent ],
-      providers:[ApiCallService],
-      imports:[HttpClientModule,CommonModule]
+      declarations: [CostDurationComponent],
+      providers: [{ provide: ApiCallService, useValue: spy }]
     })
     .compileComponents();
 
+    apiCallServiceSpy = TestBed.inject(ApiCallService) as jasmine.SpyObj<ApiCallService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(CostDurationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call apiCallService.getData() on initialization', () => {
+    const mockData = {
+      scholarship: {
+        study_commitment_text: 'The programme consists of fifteen 3-week courses for graduation. Daily classes are 3 hours, plus coursework to complete on your own time.',
+        total_value: 34900,
+        tuition: 22900,
+        stipend_per_month: 1000,
+        stipend_per_year: 12000,
+        remaining: 0,
+        internship_commitment_text: 'You will learn about the mobile games world, you will have a dedicated mentor who will help you improve your skills, and last but not least, you will help the company make better data-driven decisions.',
+        study_commitment: 3,
+        internship_commitment: 4
+      }
+    };
+    apiCallServiceSpy.getData.and.returnValue(of(mockData));
+
+    fixture.detectChanges();
+
+    expect(apiCallServiceSpy.getData).toHaveBeenCalled();
+    expect(component.study_commitment_text).toBe('The programme consists of fifteen 3-week courses for graduation. Daily classes are 3 hours, plus coursework to complete on your own time.');
+    expect(component.total_value).toBe(34900);
+    expect(component.tuition).toBe(22900);
+    expect(component.stipend_per_month).toBe(1000);
+    expect(component.stipend_per_year).toBe(12000);
+    expect(component.remaining).toBe(0);
+    expect(component.study_commitment).toBe(3);
+    expect(component.internship_commitment).toBe(4);
   });
 });
--- src/app/cost-duration/cost-duration.component.ts
+++ src/app/cost-duration/cost-duration.component.ts
@@ -7,14 +7,36 @@ import { ApiCallService } from '../shared/api-call.service';
   styleUrls: ['./cost-duration.component.scss']
 })
 export class CostDurationComponent implements OnInit {
+  study_commitment_text!:String
+  total_value:number | null = null;
+  tuition:number | null = null;
+  stipend_per_month:number | null = null;
+  stipend_per_year:number | null = null;
+  remaining:number | null = null;
+  internship_commitment_text!:string
+  study_commitment: number | null = null;
+  internship_commitment:number | null = null;
+
+
 
   constructor(private apiCallService : ApiCallService) { }
 
   ngOnInit(): void {
+    this.getData()
   }
   getData(){
     this.apiCallService.getData().subscribe((data)=>{
+      this.study_commitment_text = data.scholarship.study_commitment_text
+      this.total_value = data.scholarship.total_value
+      this.tuition = data.scholarship.tuition
+      this.stipend_per_month= data.scholarship.stipend_per_month
+      this.stipend_per_year= data.scholarship.stipend_per_year
+      this.remaining = data.scholarship.remaining
+      this.internship_commitment_text = data.scholarship.internship_commitment_text
+      this.study_commitment = data.scholarship.study_commitment
+      this.internship_commitment= data.scholarship.internship_commitment
 
+      console.log('study commitment',this.study_commitment_text)
     });
 }
 }
